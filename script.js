e16306f8972cb9a68d9f26532a7f8eabf18b3af3
Filipe Magno <filipemagno.contato@gmail.com>
2023-11-29 11:35:35 -0300

    feat: finalizacao de modo escuro e claro

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Função que altera modo escuro e modo claro na página.
 function changeMode() {
-  // "Pegando" o HTML para fazer verificações
+  // "Pegando" o HTML para realizar alterações
   const html = document.documentElement
+  // Alternando entre modo escuro e claro.
+  html.classList.toggle("light")
 
-  // Verifica se, em html, há alguma classe cujo nome é "light"
+  // Alterando foto de perfil entre modo escuro e claro
+  const image = document.querySelector(".profile img")
+  // Se houver uma classe "light" no HTML, ele fará diferentes operações
   if (html.classList.contains("light")) {
-    html.classList.remove("light")
+    // Caso haja, o atributo "src", que é onde está a imagem, irá mudar para o link abaixo.
+    image.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    html.classList.add("light")
+    // Caso não, outra imagem será escolhida.
+    image.setAttribute("src", "./assets/avatar.png")
   }
 }
