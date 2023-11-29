7732e2515853352b7d377fe67f1ff5cdb7381cb6
Filipe Magno <filipemagno.contato@gmail.com>
2023-11-29 11:17:08 -0300

    feat: modo escuro e claro através do switch

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Função que altera modo escuro e modo claro na página.
-function changeMode() {}
+function changeMode() {
+  // "Pegando" o HTML para fazer verificações
+  const html = document.documentElement
+
+  // Verifica se, em html, há alguma classe cujo nome é "light"
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+}
